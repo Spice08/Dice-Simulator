5d0da0f1f97b059936679d6f6cd4d29a52afe4d1
Mercurius13 <76958526+Mercurius13@users.noreply.github.com>
2022-10-05 17:09:13 +0530

    Update DiceSimulator.py
    Made the code difficult to break by making the options value more flexible (anything with 'y' in it is yes, 'n' in it is no)
    Code will also not break if dice number is less than 1, instead it will ask the user to enter the number of dice again
    Converted choice to randint, which is better to use when dealing with numbers as it does not require a list.

--- DiceSimulator.py
+++ DiceSimulator.py
@@ -1,19 +1,22 @@
-from random import choice
-
-numbers =  [x for x in range(1,7)]
+from random import randint
 
 while True:
     options = str(input("Do You Want To Roll The Dice?\n"))
-    if options.lower() == "yes":
-            dice_nums = int(input("How many dices you want to roll?\n"))
-            if dice_nums < 1:
-                break
+    if "y" in options.lower():
+            dice_nums = input("How many dices you want to roll?\n")
+            if dice_nums.isnumeric(): 
+                dice_nums = int(dice_nums)
             else:
-                for i in range(1,dice_nums+1):
-                    print(f"{i}st Dice Rolled => {choice(numbers)}\n")
-    elif options.lower() == "no":
+                print("Please enter a number...")
+                continue
+            if dice_nums < 1:
+                print("Sorry, you cannot roll less than one dice.")
+            else:
+                for i in range(1, dice_nums+1):
+                    print(f"{i}st Dice Rolled => {randint(1, 6)}\n")
+    elif "n" in options.lower():
         print("Closing Game... Thanks for playing!\n")
         break
     else:
-        print("Incorrect Input ! Try Again >\n")
+        print("Incorrect Input! Try Again >\n")
     
